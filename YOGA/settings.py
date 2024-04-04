9d77b9ee83ac9b912364ab9d4bb1ba2f4f1b71cb
EC2 Default User <ec2-user@ip-172-31-10-189.eu-west-1.compute.internal>
2024-04-04 15:42:57 +0000

    reloaded changes in static settings

--- YOGA/settings.py
+++ YOGA/settings.py
@@ -53,16 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
- 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
- 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
- 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'my_yoga', 'static'),
-
+    os.path.join(BASE_DIR, 'my_yoga/static'),
 ]
 
 ROOT_URLCONF = 'YOGA.urls'
