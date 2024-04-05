bf727f2bf59392dfb29a4e36de02efb1c2208b79
EC2 Default User <ec2-user@ip-172-31-10-189.eu-west-1.compute.internal>
2024-04-05 13:21:12 +0000

    webpage change

--- YOGA/urls.py
+++ YOGA/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('my_yoga/', include('my_yoga.urls')),
+    path('', include('my_yoga.urls')),
 ]
