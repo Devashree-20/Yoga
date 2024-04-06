d2d5b414f42be188b8111b3bc0d6aa6ed5c655c0
EC2 Default User <ec2-user@ip-172-31-10-189.eu-west-1.compute.internal>
2024-04-06 11:55:56 +0000

    pylint changes

--- YOGA/urls.py
+++ YOGA/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for yoga project.
+URL configuration for YOGA project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
